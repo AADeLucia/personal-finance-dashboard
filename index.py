18a07e18044cddc3b28a16157bcda8e607544c38
Alexandra <aadelucia@gmail.com>
2019-03-30 22:36:16 -0600

    added tabs and stacked bar chart

--- index.py
+++ index.py
@@ -24,19 +24,22 @@ app.layout = html.Div(children=[
             ]),
         ]),
         html.Div(id="file-status"),
-        html.Button("Import Data", id="data-import-btn")
+        html.Button("Import Data", id="data-import-btn"),
+        html.Button("Refresh Plots", id="create-plots-btn", disabled="True")
     ]),
-    
-    # Initial plot
-    html.Div(id="overview", children=[
-        html.H3("Transactions Overview"),
-        html.Div(className="row", children=[
-            html.Div(className="six columns", id="month-overview", children=[
-                html.P("Number of Transactions by Month"),
-            ])
-        ])
-    ]),
-    
+
+    # Main tabs
+    dcc.Tabs(id="main-tabs", value="transactions-overview", children=[
+        dcc.Tab(label="Transactions Overview", value="transactions-overview", children=[
+            html.Div(className="row", children=[
+                html.Div(className="six columns", id="month-overview", children=[
+                    html.P("Number of Transactions by Month"),
+                ]),
+            ]),
+        ]),
+        dcc.Tab(label="Month Breakdown", value="month-breakdown")
+    ]), # End tabs
+
 ]) # End of app layout
 
 
@@ -47,20 +50,36 @@ app.layout = html.Div(children=[
 def display_transactions(n_clicks, contents, filename):
     if n_clicks is None:
         return
-    
+
     if filename is None:
         return "No file selected"
-    
+
     df = util.parse_content(contents, filename)
     visualizer.set_transaction_data(df)
-    return f"Successfully uploaded {filename}"        
+    return f"Successfully uploaded {filename}"
+
+
+@app.callback(Output(component_id="create-plots-btn", component_property="disabled"),
+        [Input(component_id="data-import-btn", component_property="n_clicks")],
+        [State(component_id="data-upload", component_property="contents"),
+        State(component_id="data-upload", component_property="filename")])
+def enable_button(n_clicks, contents, filename):
+    if n_clicks is None:
+        return "True"
+
+    if filename is None:
+        return "True"
+
+    return "False"
 
 
 @app.callback(Output(component_id="month-overview", component_property="children"),
-        [Input(component_id="data-import-btn", component_property="n_clicks")])
+        [Input(component_id="create-plots-btn", component_property="n_clicks")])
 def plot_month_overview(n_clicks):
+    print("here")
     if n_clicks is None:
         return html.P("Upload data to see month overview")
+    print("Recognized click")
     return dcc.Graph(figure=visualizer.plot_agreggate_transactions_by_month())
 
 
--- visualizer.py
+++ visualizer.py
@@ -9,11 +9,11 @@ class Visualizer:
         self.transaction_df = None
         return
 
-    
+
     def set_transaction_data(self, transactions_df):
         """
         Sets the transaction data
-        
+
         transactions : Pandas dataframe from Utility parse_content. From a Mint CSV.
         """
         self.transaction_df = transactions_df
@@ -21,25 +21,32 @@ class Visualizer:
         self.transaction_df["Date"] = self.transaction_df["Date"].map(lambda x: dt.datetime.strptime(x, "%m/%d/%Y"))
         # Add month info
         self.transaction_df["month"] = self.transaction_df["Date"].map(lambda x: dt.date(x.year, x.month, 1))
-    
-    
+
+
     def plot_agreggate_transactions_by_month(self):
+        print("Graphing")
         if self.transaction_df is None:
             return
-        
-        data = self.transaction_df.groupby("month").Date.count()
-    
-        trace = go.Bar(
-            x = data.index,
-            y = data
-        )
-        
+
+        traces = []
+        for account, df in self.transaction_df.groupby("Account Name"):
+            data = df.groupby("month").Date.count()
+            trace = go.Bar(
+                x = data.index,
+                y = data,
+                name = account
+            )
+            traces.append(trace)
+
         layout = go.Layout(
-            title = "Total Number of Transactions"
+            title = "Total Number of Transactions",
+            barmode = "stack",
+            xaxis = dict(title="Month"),
+            yaxis = dict(title="Number of Transactions")
         )
-        fig = go.Figure(data=[trace], layout=layout)
+        fig = go.Figure(data=traces, layout=layout)
         return fig
-    
+
 
 if __name__ == "__main__":
     df = pd.read_csv("transactions.csv")
@@ -47,5 +54,3 @@ if __name__ == "__main__":
     visualizer.set_transaction_data(df)
     #print(visualizer.transaction_df.columns)
     print(visualizer.plot_agreggate_transactions_by_month())
-    
-    
