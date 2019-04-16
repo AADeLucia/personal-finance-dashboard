b05bf13a59db7aeb28265047e1ed46ab50579f44
Alexandra Ann DeLucia <aadelucia@lanl.gov>
2019-04-15 18:10:11 -0600

    updated category list and added exploration plot

--- index.py
+++ index.py
@@ -18,7 +18,7 @@ app.layout = html.Div(children=[
     html.H1("Personal Finance Dashboard"),
 
     # Data import
-    html.Div(id="data-import", className="row", children=[
+    html.Div(id="data-import-container", className="row", children=[
         html.Div(className="six columns", children=[
             dcc.Upload(id="data-upload"),
         ]),
@@ -29,9 +29,46 @@ app.layout = html.Div(children=[
     ]),
 
     # Main tabs
-    dcc.Tabs(id="main-tabs", value="transactions-overview", children=[
+    dcc.Tabs(id="main-tabs", value="transaction-explorer", children=[
         dcc.Tab(label="Transactions Overview", value="transactions-overview", id="transactions-overview"),
-        dcc.Tab(label="Month Breakdown", value="month-breakdown")
+        dcc.Tab(label="Month Breakdown", value="month-breakdown"),
+        dcc.Tab(label="Explorer", value="transaction-explorer", children=[
+            html.Div(className="row", children=[
+                html.Div(className="four columns", children=[
+                    html.P("Date Range"),
+                    dcc.DatePickerRange(id="explorer-date-picker")
+                ]),
+                html.Div(className="four columns", children=[
+                    html.P("Accounts"),
+                    dcc.Checklist(id="explorer-accounts-selector", options=[], values=[])
+                ]),
+                html.Div(className="four columns", children=[
+                    html.P("Transaction Type"),
+                    dcc.Checklist(id="explorer-type-selector", options=[
+                        {"label": "Debit", "value": "debit"},
+                        {"label": "Credit", "value": "credit"}
+                    ], values=["debit", "credit"])
+                ]),
+            ]),
+            html.Div(className="row", children=[
+                html.Div(className="four columns", children=[
+                    html.P("Category"),
+                    dcc.Dropdown(id="explorer-category-selector", options=[{"label": "All", "value": "All"}]+[{"label": i, "value": i} for i in visualizer.get_category_list()], value=["All"], multi=True)
+                ]),
+                html.Div(className="four columns", children=[
+                    html.P("Subcategory"),
+                    dcc.Dropdown(id="explorer-subcategory-selector", options=[{"label": "All", "value": "All"}]+[{"label": i, "value": i} for i in visualizer.get_subcategory_list()], value=["All"], multi=True)
+                ]),
+                html.Div(className="four columns", children=[
+                    html.P("Merchants"),
+                    dcc.Dropdown(id="explorer-merchant-selector", options=[], value=[], multi=True)
+                ])
+            ]),
+            html.Div(id="explorer-btn-container", className="row", children=[
+                html.Button("Submit", id="explorer-submit-btn")
+            ]),
+            html.Div(className="row", id="explorer-plot")
+        ])
     ]), # End tabs
 
 ]) # End of app layout
@@ -77,6 +114,90 @@ def plot_overview(n_clicks):
             ])
         ])
     ]
+    return children
+
+
+@app.callback([Output(component_id="explorer-accounts-selector", component_property="options"),
+               Output(component_id="explorer-accounts-selector", component_property="values")],
+              [Input(component_id="data-import-btn", component_property="n_clicks")])
+def update_explorer_accounts_options(n_clicks):
+    default = [], []
+    if n_clicks is None:
+        return default
+    accounts_list = visualizer.get_accounts_list()
+    
+    if accounts_list is None:
+        return default
+    
+    options = [{"label": i, "value": i} for i in accounts_list]
+    return options, accounts_list
+
+
+@app.callback([Output(component_id="explorer-merchant-selector", component_property="options"),
+               Output(component_id="explorer-merchant-selector", component_property="value")],
+              [Input(component_id="data-import-btn", component_property="n_clicks")])
+def update_explorer_merchant_options(n_clicks):
+    default = [], []
+    if n_clicks is None:
+        return default
+    merchant_list = visualizer.get_merchant_list()
+    
+    if merchant_list is None:
+        return default
+    
+    options = [{"label": "All", "value": "All"}] + [{"label": i, "value": i} for i in merchant_list]
+    return options, ["All"]
+
+
+@app.callback([Output(component_id="explorer-date-picker", component_property="min_date_allowed"),
+               Output(component_id="explorer-date-picker", component_property="max_date_allowed"), 
+               Output(component_id="explorer-date-picker", component_property="initial_visible_month"),
+               Output(component_id="explorer-date-picker", component_property="start_date"),
+               Output(component_id="explorer-date-picker", component_property="end_date")],
+              [Input(component_id="data-import-btn", component_property="n_clicks")])
+def update_explorer_accounts_options(n_clicks):
+    default_min = dt.date(2016, 1, 1)
+    default_max = dt.date.today()
+    initial_visible_month = dt.date.today()
+    start_date = default_min
+    end_date = default_max
+    if n_clicks is None:
+        return default_min, default_max, initial_visible_month, start_date, end_date
+    
+    date_range = visualizer.get_date_range()
+    if date_range is None:
+        return default_min, default_max, initial_visible_month, start_date, end_date
+    
+    start_date = date_range[0]
+    end_date = date_range[1]
+    return start_date, end_date, initial_visible_month, start_date, end_date
+    
+
+@app.callback(Output(component_id="explorer-plot", component_property="children"),
+             [Input(component_id="explorer-submit-btn", component_property="n_clicks")],
+             [State(component_id="explorer-date-picker", component_property="start_date"),
+              State(component_id="explorer-date-picker", component_property="end_date"),
+              State(component_id="explorer-accounts-selector", component_property="values"),
+              State(component_id="explorer-type-selector", component_property="values"),
+              State(component_id="explorer-category-selector", component_property="value"),
+              State(component_id="explorer-subcategory-selector", component_property="value"),
+              State(component_id="explorer-merchant-selector", component_property="value")
+             ])
+def update_explorer_plot(n_clicks, start_date, end_date, accounts, transaction_types, categories, subcategories, merchants):
+    children = html.Div("Enter options and click Submit")
+    if n_clicks is None:
+        return children
+    
+    if "All" in categories:
+        categories = None
+    if "All" in subcategories:
+        subcategories = None
+    if "All" in merchants:
+        merchants = None
+    
+    children = html.Div(children=[
+        dcc.Graph(figure=visualizer.plot_explorer(start_date, end_date, accounts, transaction_types, categories, subcategories, merchants))
+    ])
     
     return children
 
--- visualizer.py
+++ visualizer.py
@@ -8,10 +8,10 @@ class Visualizer:
     def __init__(self):
         self.transaction_df = None
         with open("mint_subcategories", "r") as f:
-            self.subcategories = {i.split(",")[0].strip():i.split(",")[1].strip() for i in f.readlines()}
+            self.category_lookup = {i.split(",")[0].strip():i.split(",")[1].strip() for i in f.readlines()}
+        self.subcategories = list(set(self.category_lookup.keys()))
         with open("mint_categories", "r") as f:
             self.categories = [i.strip() for i in f.readlines()]
-        return
 
 
     def set_transaction_data(self, transactions_df):
@@ -25,6 +25,9 @@ class Visualizer:
         self.transaction_df["Date"] = self.transaction_df["Date"].map(lambda x: dt.datetime.strptime(x, "%m/%d/%Y"))
         # Add month info
         self.transaction_df["month"] = self.transaction_df["Date"].map(lambda x: dt.date(x.year, x.month, 1))
+        # Add main category label
+        self.transaction_df["subcategory"] = self.transaction_df["Category"]
+        self.transaction_df["Category"] = self.transaction_df["Category"].map(lambda x: self.category_lookup[x])
 
 
     def plot_agreggate_transactions_by_month(self):
@@ -51,16 +54,20 @@ class Visualizer:
         return fig
     
     
+    def plot_transactions_for_month(self):
+        return
+    
+    
     def plot_category_pie(self):
         if self.transaction_df is None:
             return
         
-        labels = self.transaction_df.Category.unique()
+        labels = self.categories
         values = [len(self.transaction_df[self.transaction_df.Category==c]) for c in labels]
         trace = go.Pie(
             labels = labels,
             values = values,
-            textinfo=None
+            textinfo = "none"
         )
         
         layout = go.Layout(
@@ -70,7 +77,82 @@ class Visualizer:
         fig = go.Figure(data=[trace], layout=layout)
         return fig
 
-
+    
+    def plot_explorer(self, start_date, end_date, accounts, transaction_types, categories, subcategories, merchants):
+        if self.transaction_df is None:
+            return
+        
+        # Limit transactions by date
+        df = self.transaction_df[(self.transaction_df.Date >= start_date) & (self.transaction_df.Date <= end_date)]
+        
+        # Limit by account
+        if accounts is not None:
+            df = df[df["Account Name"].isin(accounts)]
+        
+        # Limit by transaction type
+        df = df[df["Transaction Type"].isin(transaction_types)]
+        
+        # Limit by categories
+        if categories is not None:
+            df = df[df["Category"].isin(categories)]
+        
+        # Limit by subcategories
+        if subcategories is not None:
+            df = df[df["subcategory"].isin(subcategories)]
+        
+        # Limit by merchants
+        if merchants is not None:
+            df = df[df["subcategory"].isin(subcategories)]
+        
+        # Put credit as negative
+        if "credit" in transaction_types:
+            df["Amount"] = df.apply(lambda x: -x.Amount if x["Transaction Type"]=="credit" else x.Amount, axis=1)
+        
+        # Plot
+        trace = go.Scatter(
+            x = df.Date,
+            y = df.Amount,
+            text = df.Description,
+            mode = "markers"
+        )
+        
+        layout = go.Layout(
+            title = "Transactions", 
+            yaxis = dict(title="Charge Amount", hoverformat="$.2f"),
+            xaxis = dict(title="Date"),
+            hovermode = "closest"
+        )
+        
+        fig = go.Figure(data=[trace], layout=layout)
+        return fig
+        
+        
+    def get_category_list(self):
+        return self.categories
+    
+    
+    def get_subcategory_list(self):
+        return self.subcategories
+    
+    
+    def get_accounts_list(self):
+        if self.transaction_df is None:
+            return None
+        return self.transaction_df["Account Name"].unique()
+    
+    
+    def get_merchant_list(self):
+        if self.transaction_df is None:
+            return None
+        return self.transaction_df["Description"].unique()
+    
+    
+    def get_date_range(self):
+        if self.transaction_df is None:
+            return None
+        return self.transaction_df["Date"].min(), self.transaction_df["Date"].max()
+        
+        
 if __name__ == "__main__":
     df = pd.read_csv("transactions.csv")
     visualizer = Visualizer()
